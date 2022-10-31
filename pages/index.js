3b9f586143cc25dd54c29481cf7f4797655f3ad2
Mohammad Zeeshan Jawed <zeeshan8281@gmail.com>
2022-10-31 13:35:39 +0530

    :sparkles: Footer added
    Footer added along with utilizations

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,12 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import membercard from '../components/Membercard'
 import Membercard from '../components/Membercard'
+import Footer from '../components/footer'
 export default function Home() {
   return (
     <div>
       <Membercard/>
+      <Footer/>
     </div>
   )
 }
---
+++ utils/icons/index.jsx
@@ -0,0 +1,4 @@
+export { default as Mail } from "./mail";
+export { default as Linkedin } from "./linkedin";
+export { default as Twitter } from "./twitter";
+export { default as Instagram } from "./instagram";
