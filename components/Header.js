0a7b7febebcd55c3f834198a73d081ca02ba6c05
Aryan Kumar <ay2475@srmist.edu.in>
2022-11-01 19:23:13 +0530

    Header done

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+
+        <div className=' flex justify-center'>
+
+            <picture className=' flex justify-center'>
+                <img  className=' w-3/12' src='/logo.png' alt=''></img>
+            </picture>
+        </div>
+    </div>
+  )
+}
